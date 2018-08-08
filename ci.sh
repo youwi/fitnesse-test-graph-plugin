bbaeaf831fb188021a2321968fe7e0ca5404d3ec
yu@sec.com <yu@sec.com>
2018-08-08 18:10:07 +0800

    注释无用代码

--- ci.sh
+++ ci.sh
@@ -1,3 +1,3 @@
 scp build/libs/*.jar test.jihui.in:/root/fitnesse/plugins/
 curl test.jihui.in:9090/any?restart
-ssh test.jihui.in 'cd /root/fitnesse  && /root/fitnesse/_start.sh&'
+ssh test.jihui.in 'cd /root/fitnesse  && /root/fitnesse/_start.sh >log.log 2>&1'
--- src/main/java/fitnesse/plugin/graph/PageContentsNormalNameSymbolType.java
+++ src/main/java/fitnesse/plugin/graph/PageContentsNormalNameSymbolType.java
@@ -2,9 +2,6 @@ package fitnesse.plugin.graph;
 
 import fitnesse.html.HtmlTag;
 import fitnesse.html.HtmlUtil;
-import fitnesse.wiki.PageData;
-import fitnesse.wiki.PageType;
-import fitnesse.wiki.WikiImportProperty;
 import fitnesse.wikitext.parser.*;
 
 import java.util.List;
@@ -19,47 +16,49 @@ import static fitnesse.util.BeanUtil.getObjectByPath;
  */
 public class PageContentsNormalNameSymbolType extends SymbolType implements Rule, Translation {
 
-    public PageContentsNormalNameSymbolType() {
-        super("ContentsNormalName");
-        wikiMatcher(new Matcher().startLineOrCell().string("!contentx"));
-        wikiRule(this);
-        htmlTranslation(this);
+  public PageContentsNormalNameSymbolType() {
+    super("ContentsNormalName");
+    wikiMatcher(new Matcher().startLineOrCell().string("!contentx"));
+    wikiRule(this);
+    htmlTranslation(this);
+  }
+
+  @Override
+  public Maybe<Symbol> parse(Symbol current, Parser parser) {
+    Symbol body = parser.parseToEnd(SymbolType.Newline);
+    for (Symbol option : body.getChildren()) {
+      if (option.isType(SymbolType.Whitespace)) continue;
+      if (!option.getContent().startsWith("-")) return Symbol.nothing;
+      current.add(option);
     }
 
-    @Override
-    public Maybe<Symbol> parse(Symbol current, Parser parser) {
-        Symbol body = parser.parseToEnd(SymbolType.Newline);
-        for (Symbol option : body.getChildren()) {
-            if (option.isType(SymbolType.Whitespace)) continue;
-            if (!option.getContent().startsWith("-")) return Symbol.nothing;
-            current.add(option);
-        }
+    return new Maybe<>(current);
+  }
 
-        return new Maybe<>(current);
-    }
-
-    @Override
-    public String toTarget(Translator translator, Symbol symbol) {
-        ContentsItemBuilder itemBuilder = new PageCinContentsItemBuilder(symbol, 1, translator.getPage());
-        HtmlTag contentsDiv = new HtmlTag("div");
-        contentsDiv.addAttribute("class", "contents");
-        contentsDiv.add(HtmlUtil.makeBold("Contents:"));
-        contentsDiv.add(itemBuilder.buildLevel(translator.getPage()));
-        // ((HtmlTag) ((HtmlTag) contentsDiv.childTags.last.item).childTags.last.item).childTags
-        try {
-            List<HtmlTag> list = (List) getObjectByPath(contentsDiv, "childTags");
-            HtmlTag tag = list.get(list.size() - 1);
-            list = (List) getObjectByPath(tag, "childTags");
-            for (HtmlTag htmlTag : list) {
-                System.out.println(htmlTag);
-                //htmlTag.
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return contentsDiv.html();
-    }
+  @Override
+  public String toTarget(Translator translator, Symbol symbol) {
+    long stime = System.currentTimeMillis();
+    ContentsItemBuilder itemBuilder = new PageCinContentsItemBuilder(symbol, 1, translator.getPage());
+    HtmlTag contentsDiv = new HtmlTag("div");
+    contentsDiv.addAttribute("class", "contents");
+    contentsDiv.add(HtmlUtil.makeBold("Contents:"));
+    contentsDiv.add(itemBuilder.buildLevel(translator.getPage()));
+    // ((HtmlTag) ((HtmlTag) contentsDiv.childTags.last.item).childTags.last.item).childTags
+    //    try {
+    //List<HtmlTag> list = (List) getObjectByPath(contentsDiv, "childTags");
+    //HtmlTag tag = list.get(list.size() - 1);
+    // list = (List) getObjectByPath(tag, "childTags");
+    //      for (HtmlTag htmlTag : list) {
+    //        System.out.println(htmlTag);
+    //        //htmlTag.
+    //
+    //      }
+    //    } catch (Exception e) {
+    //      e.printStackTrace();
+    //    }
+    System.out.println("!contentx timing: " + (System.currentTimeMillis() - stime));
+    return contentsDiv.html();
+  }
 
 
 }
